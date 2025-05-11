d87ed9f063eae5fee173d152c1d902b5ff76133d
Achico Whyner <achicowhyner19@gmail.com>
2025-05-11 19:05:53 +0800

    wtf did i do
    ewan

---
+++ src/main/java/com/CareNet/CN/controller/PatientDTO.java
@@ -0,0 +1,11 @@
+package com.CareNet.CN.model;
+
+public class PatientDTO {
+    private String name;
+    private String datetime;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getDatetime() { return datetime; }
+    public void setDatetime(String datetime) { this.datetime = datetime; }
+}
